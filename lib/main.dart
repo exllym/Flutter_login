9573afb122fa64eb9f5a28892a60c169da5dc4df
Sirojuddin_Abbas_Masluchin <exozi434@gmail.com>
2026-09-08 02:28:52 +0000

    Flutter_login

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(Coba());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class Coba extends StatefulWidget {
   @override
+  State<Coba> createState() => Coba_State();
+}
+
+class Coba_State extends State<Coba> {
+  TextEditingController a = TextEditingController();
+  String nama = "";
+
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [
+            TextField(
+              controller: a,
+              decoration: InputDecoration(
+                labelText: "Nama",
+                hintText: ("Masukkan Nama"),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    a.text;
+                  });
+                },
+                child: Text("Ok")),
+            Text(a.text),
+          ],
         ),
       ),
     );
